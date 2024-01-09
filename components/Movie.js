9e98cf84d56442a363a0420724e6cdcbe9f1a5ac
Dimitri <dfrancillette@gmail.com>
2024-01-09 11:11:03 +0100

    movies test data added and displayed

--- components/Movie.js
+++ components/Movie.js
@@ -2,33 +2,34 @@ import styles from '../styles/Movie.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-function Movie() {
+function Movie(props) {
+    const stars = [];
+
+    for (let i = 0; i < 10; i++) {
+        if (i< Math.floor(props.voteAverage)) {
+            stars.push(<FontAwesomeIcon icon={faStar} style={{color: "#f1c40f"}}/>)
+        } else {
+            stars.push(<FontAwesomeIcon icon={faStar} />)
+        }
+        
+    }
     return (
         <div className={styles.globalDiv}>
             <div>
-                <img className={styles.img} src="/poster.jpg" alt="Poster" />
+                <img className={styles.img} src={props.poster} alt={props.title} />
             </div>
             <div>
-                <h2 className={styles.title}>Movie</h2>
+                <h2 className={styles.title}>{props.title}</h2>
             </div>
             <div>
-                <p className={styles.description}>Description</p>
+                <p className={styles.description}>{props.overview}</p>
             </div>
             <div className={styles.ratingDiv}>
                 <div className={styles.rating}>
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
+                    {stars}
                 </div>
                 <div className={styles.votes}>
-                    <p>(vote count)</p>
+                    <p>{props.voteCount}</p>
                 </div>
                 
             </div>
